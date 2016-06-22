8037ec7d59385b2bad3702174d9cb4c58db882b2
Gun-Hee Jo <ckheee93@naver.com>
2016-06-22 16:11:01 +0900

    Added fastBtn

--- Assets/Script/Title.cs
+++ Assets/Script/Title.cs
@@ -4,19 +4,32 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class Title : MonoBehaviour {
 	Image fade;
+    public Button fastBtn;
 	// Use this for initialization
-	void Start () {
-		//StartCoroutine (fadeIn ());
+	void Awake () {
+        fade = GameObject.Find("Fade").GetComponent<Image>();
+        fade.color = new Color(0, 0,0,255);
+        fade.CrossFadeAlpha(0,1.0f,false);
+        StartCoroutine (fadeIn ());
 
 	}
 	IEnumerator fadeIn(){
 		while (fade.color.a < 255) {
 			yield return new WaitForSeconds (0.1f);
+            
 		}
 	}
 	// Update is called once per frame
 	void Update () {
 	}
+    public void fastBtnDown()
+    {
+        if(Time.timeScale < 2)
+            Time.timeScale = 2;
+        else
+            Time.timeScale = 1;
+      
+    }
 	public void nextBtnDown(){
 		SceneManager.LoadScene (++userData.curStageNum+3);
 	}
